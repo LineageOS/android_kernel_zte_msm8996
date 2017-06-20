92d87dbaf9a3923bd91d97452d716eb3c4b356a0
c457 <android.c357@gmail.com>
2017-06-20 13:30:09 -0500

    ASoC: msm: Import ZTE changes

--- sound/soc/msm/qdsp6v2/msm-dai-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-dai-q6-v2.c
@@ -3351,7 +3351,9 @@ static struct snd_soc_dai_driver msm_dai_q6_mi2s_dai[] = {
 			.rates = SNDRV_PCM_RATE_48000 | SNDRV_PCM_RATE_8000 |
 			SNDRV_PCM_RATE_16000 | SNDRV_PCM_RATE_96000 |
 			SNDRV_PCM_RATE_192000,
-			.formats = SNDRV_PCM_FMTBIT_S16_LE,
+			.formats = SNDRV_PCM_FMTBIT_S16_LE |
+				SNDRV_PCM_FMTBIT_S24_LE |
+				SNDRV_PCM_FMTBIT_S24_3LE, // ZTE_chenjun
 			.rate_min =     8000,
 			.rate_max =     192000,
 		},
