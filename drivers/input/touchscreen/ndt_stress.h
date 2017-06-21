194fa3256d64d98809d92c0842998001659e4aa1
c457 <android.c357@gmail.com>
2017-06-21 11:08:43 -0500

    drivers: input: Import ZTE touchscreen changes
    Change-Id: If53008c3718bd194d649a6ab4da6995ee4c97605

---
+++ drivers/input/touchscreen/ndt_stress.h
@@ -0,0 +1,12 @@
+#ifndef _NDT_FORCE_TOUCH_H
+#define _NDT_FORCE_TOUCH_H
+
+int ndt_stress_resume(void);
+int ndt_stress_suspend(void);
+int ndt_stress_set_vlaue(unsigned short x1,  unsigned short y1, unsigned char status);
+int ndt_stress_get_vlaue(unsigned int *value);
+int ndt_get_max_coord(unsigned short max_x,  unsigned short max_y);
+void ndt_i2c_comm_lock(void);
+void ndt_i2c_comm_unlock(void);
+
+#endif
