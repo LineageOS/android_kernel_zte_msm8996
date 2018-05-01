62fc34c42736ad795751b0a1c83c7b766c656dad
c457 <android.c357@gmail.com>
2018-05-01 06:10:12 -0500

    drivers: input: fingerprint: Import ZTE Oreo GF3118M changes

--- drivers/input/fingerprint/goodix/gf_spi.h
+++ drivers/input/fingerprint/goodix/gf_spi.h
@@ -4,7 +4,7 @@
 #include <linux/types.h>
 #include <linux/notifier.h>
 /**********************************************************/
-enum FP_MODE{
+enum FP_MODE {
 	GF_IMAGE_MODE = 0,
 	GF_KEY_MODE,
 	GF_SLEEP_MODE,
@@ -18,32 +18,31 @@ struct gf_key {
 };
 
 
-struct gf_key_map
-{
-    char *name;
-    unsigned short val;
+struct gf_key_map {
+	char *name;
+	unsigned short val;
 };
 
-#define  GF_IOC_MAGIC         'G'
+#define  GF_IOC_MAGIC		  'G'
 #define  GF_IOC_DISABLE_IRQ	_IO(GF_IOC_MAGIC, 0)
 #define  GF_IOC_ENABLE_IRQ	_IO(GF_IOC_MAGIC, 1)
-#define  GF_IOC_SETSPEED    _IOW(GF_IOC_MAGIC, 2, unsigned int)
-//#define  GF_IOC_RESET       _IO(GF_IOC_MAGIC, 3)
-#define  GF_IOC_RESET       _IOW(GF_IOC_MAGIC, 3, unsigned int)
-#define  GF_IOC_COOLBOOT    _IO(GF_IOC_MAGIC, 4)
+#define  GF_IOC_SETSPEED	_IOW(GF_IOC_MAGIC, 2, unsigned int)
+/*#define  GF_IOC_RESET		  _IO(GF_IOC_MAGIC, 3)*/
+#define  GF_IOC_RESET		_IOW(GF_IOC_MAGIC, 3, unsigned int)
+#define  GF_IOC_COOLBOOT	_IO(GF_IOC_MAGIC, 4)
 #define  GF_IOC_SENDKEY    _IOW(GF_IOC_MAGIC, 5, struct gf_key)
 #define  GF_IOC_CLK_READY  _IO(GF_IOC_MAGIC, 6)
 #define  GF_IOC_CLK_UNREADY  _IO(GF_IOC_MAGIC, 7)
-#define  GF_IOC_PM_FBCABCK  _IO(GF_IOC_MAGIC, 8)
+#define  GF_IOC_PM_FBCABCK	_IO(GF_IOC_MAGIC, 8)
 #define  GF_IOC_POWER_ON   _IO(GF_IOC_MAGIC, 9)
 #define  GF_IOC_POWER_OFF  _IO(GF_IOC_MAGIC, 10)
 
-#define  GF_IOC_MAXNR    11
+#define  GF_IOC_MAXNR	 11
 
-//#define AP_CONTROL_CLK       1
-#define  USE_PLATFORM_BUS     1
-//#define  USE_SPI_BUS	1
-#define GF_FASYNC   1	/*If support fasync mechanism.*/
+/*#define AP_CONTROL_CLK	   1*/
+#define  USE_PLATFORM_BUS	  1
+/*#define  USE_SPI_BUS	1*/
+#define GF_FASYNC	1	/*If support fasync mechanism.*/
 struct gf_dev {
 	dev_t devt;
 	struct list_head device_entry;
@@ -72,7 +71,7 @@ struct gf_dev {
 	char fb_black;
 };
 
-int gf_parse_dts(struct gf_dev* gf_dev);
+int gf_parse_dts(struct gf_dev *gf_dev);
 void gf_cleanup(struct gf_dev *gf_dev);
 
 int gf_power_on(struct gf_dev *gf_dev);
